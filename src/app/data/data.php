c0bd4d9eb8d1b0a72e47a66b178690f8037418be
Jason Carr <jason@argyletechnologygroup.com>
2016-10-12 16:36:23 +0000

    HUNTBID-7 Initial implementation of lead data. Reads from static data until authentication is finished.

---
+++ src/app/data/data.php
@@ -0,0 +1,29 @@
+<?php namespace App\Data;
+
+class Lead implements \JsonSerializable {
+    private /* array */ $attributes = array() ;
+
+    private $attributeMap = array(
+        'Date Received' => 'date_received',
+        'Lead Name' => 'name',
+        'Email/Phone' => 'email_phone',
+        'Source' => 'source',
+        'Property Address' => 'property_address',
+        'Agent Assigned' => 'agent',
+        'Office' => 'office',
+        'Notes' => 'notes',
+    );
+
+    public function __construct(/* array */ $data) {
+        foreach($this->attributeMap as $dataKey => $attributeName) {
+            $this->attributes[$attributeName] = '';
+            if (array_key_exists($dataKey, $data)) {
+                $this->attributes[$attributeName] = $data[$dataKey];
+            }
+        }
+    }
+
+    public function jsonSerialize() {
+        return $this->attributes;
+    }
+}
